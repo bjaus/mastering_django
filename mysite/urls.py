3b18f563efdd0ed963a58d31ba9e5da41d4e5b4f
Jaus <brandonjaus@remingtonhotels.com>
2017-01-30 15:29:15 -0600

    Added URLconf for root

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from mysite.views import hello
 
 urlpatterns = [
+    url(r'^$', hello),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^hello/$', hello),
 ]
