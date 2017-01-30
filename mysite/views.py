3996f3a3255b6f85261603973bd639a5acb2f34c
Jaus <brandonjaus@remingtonhotels.com>
2017-01-30 15:44:39 -0600

    Added current_datetime view

--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
+import datetime
 
 def hello(request):
     return HttpResponse("Hello World")
+
+    
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now {}.</body></html>".format(now)
+    return HttpResponse(html)
