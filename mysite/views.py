c75b41455c160c68293d8ba69dd8b033eaf7ffdd
bjaus <brandon.jaus@gmail.com>
2017-01-31 04:47:42 -0600

    Wrote hours plus view function

--- mysite/views.py
+++ mysite/views.py
@@ -9,3 +9,15 @@ def current_datetime(request):
     now = datetime.datetime.now()
     html = "<html><body>It is now {}.</body></html>".format(now)
     return HttpResponse(html)
+    
+    
+def hours_ahead(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError as ve:
+        raise Http404()
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    html = '<html><body>In {} hour(s), it will be {}.</body></html>'.format(
+        offset, dt
+    )
+    return HttpResponse(html)
