70588dcc191503eae01344176680a19be3945c3a
Jaus <brandonjaus@remingtonhotels.com>
2017-01-31 08:46:30 -0600

    Learning about DTP

--- mysite/settings.py
+++ mysite/settings.py
@@ -55,7 +55,8 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # 'BACKEND': 'django.template.backends.jinja2.Jinja2', # Used for Jinja template engine
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,5 @@
-from django.template import Template, Context
+from django.template.loader import get_template
+from django.template import Context
 from django.http import HttpResponse
 import datetime
 
@@ -8,7 +9,7 @@ def hello(request):
     
 def current_datetime(request):
     now = datetime.datetime.now()
-    t = Template("<html><body>It is now {{ current_date }}.</body></html>")
+    t = get_template('current_datetime.html')
     html = t.render(Context({ 'current_date': now }))
     return HttpResponse(html)
     
