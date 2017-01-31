ede4090c9d3f2576c07403949023c9d8422aedd3
Jaus <brandonjaus@remingtonhotels.com>
2017-01-31 09:09:04 -0600

    Utilized render from django.shortcuts

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,4 @@
-from django.template.loader import get_template
-from django.template import Context
-from django.http import HttpResponse
+from django.shortcuts import render
 import datetime
 
 def hello(request):
@@ -9,9 +7,11 @@ def hello(request):
     
 def current_datetime(request):
     now = datetime.datetime.now()
-    t = get_template('current_datetime.html')
-    html = t.render(Context({ 'current_date': now }))
-    return HttpResponse(html)
+    return render(
+        request, 
+        'current_datetime.html', 
+        {'current_datetime': now}
+    )
     
     
 def hours_ahead(request, offset):
