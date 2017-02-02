66ee36a2b8e4a292d547780e82ca2a385be60fa4
Jaus <brandonjaus@remingtonhotels.com>
2017-02-02 08:36:38 -0600

    Added __str__ method to each of the models

--- books/models.py
+++ books/models.py
@@ -8,11 +8,17 @@ class Publisher(models.Model):
     country = models.CharField(max_length=50)
     website = models.URLField()
     
+    def __str__(self):
+        return self.name
+    
     
 class Author(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=40)
     email = models.EmailField()
+    
+    def __str__(self):
+        return u'{} {}'.format(self.first_name, self.last_name)
     
     
 class Book(models.Model):
@@ -20,3 +26,6 @@ class Book(models.Model):
     authors = models.ManyToManyField(Author)
     publisher = models.ForeignKey(Publisher)
     publication_date = models.DateField()
+    
+    def __str__(self):
+        return self.title
