5abacec5a0c8d4e7853099256231465352b265dd
Jaus <brandon.jaus@gmail.com>
2017-02-02 13:16:10 -0600

    Removed old URLconfs from beging of the book

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,11 +17,7 @@ from django.contrib import admin
 from books import views
 
 urlpatterns = [
-    # url(r'^$', hello),
     url(r'^admin/', include(admin.site.urls)),
-    # url(r'^hello/$', hello),
-    # url(r'^time/$', current_datetime),
-    # url(r'^time/plus/(\d{1,2})/$', hours_ahead),
     url(r'^search-form/$', views.search_form),
     url(r'^search/$', views.search),
 ]
