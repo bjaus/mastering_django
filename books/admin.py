f8d8806d94e9abc8524eef53a6e46359c28369a1
Jaus <brandonjaus@remingtonhotels.com>
2017-02-02 09:54:23 -0600

    Created list displays for books models

--- books/admin.py
+++ books/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from .models import Publisher, Author, Book
 
+class PublisherAdmin(admin.ModelAdmin):
+    list_display = ('name', 'city', 'state_province', 'country')
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
+    
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'authors', 'publisher', 'publication_date')
+
+
 admin.site.register(Publisher)
-admin.site.register(Author)
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Book)
