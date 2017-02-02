2fd8bb45796ef4b8a7526394bb2e42d3ef2fd0ca
Jaus <brandonjaus@remingtonhotels.com>
2017-02-02 10:07:16 -0600

    Added search fields for models

--- books/admin.py
+++ books/admin.py
@@ -3,14 +3,17 @@ from .models import Publisher, Author, Book
 
 class PublisherAdmin(admin.ModelAdmin):
     list_display = ('name', 'city', 'state_province', 'country')
+    search_fields = ('name', 'city', 'state_province', 'country')
 
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email')
+    search_fields = ('first_name', 'last_name')
     
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'publisher', 'publication_date')
+    search_fields = ('title', 'publisher', 'publication_date')
 
 
 admin.site.register(Publisher, PublisherAdmin)
