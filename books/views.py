21ff36e1754d1384d10cb262141a24a92a6ebff5
Jaus <brandon.jaus@gmail.com>
2017-02-03 09:07:00 -0600

    Sorting by pub date desc

--- books/views.py
+++ books/views.py
@@ -10,15 +10,7 @@ def search_form(request):
 def search(request):
     if 'q' in request.GET and request.GET['q']:
         q = request.GET['q']
-        books = Book.objects.filter(title__icontains=q).order_by('title')
+        books = Book.objects.filter(title__icontains=q).order_by('-publication_date')
         return render(request, 'search_result.html', {'books': books, 'query': q})
     else:
         return HttpResponse('Please submit a search term.')
-
-# def display_meta(request):
-    # values = request.META.items()
-    # values.sort()
-    # html = []
-    # for k, v in values:
-        # html.append('<tr><td>{}</td><td>{}</td></tr>'.format(k, v))
-    # return HttpResponse('<table>{}</table>'.format(html))
